bcd940215867a7e50bf38032c3edda9a340e2554
sanaa <Sanaakumar07@gmail.com>
2025-09-04 10:46:57 +0530

    Initial Commit

---
+++ prime numbers/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  prime numbers
+//
+//  Created by Sanaa Kumar on 04/09/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
